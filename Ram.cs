5330c6c1b18417ab3db9548554d710401d68a200
dudley-codes <alexdudley@me.com>
2021-06-07 13:59:22 -0500

    exercise complete

--- Ram.cs
+++ Ram.cs
@@ -5,8 +5,7 @@ namespace Garage
   public class Ram : Vehicle // Gas powered truck
   {
     public double FuelCapacity { get; set; }
-    public string MainColor { get; set; }
-    public int MaximumOccupancy { get; set; }
+
 
     public void RefuelTank()
     {
--- Zero.cs
+++ Zero.cs
@@ -5,8 +5,7 @@ namespace Garage
   public class Zero : Vehicle // Electric motorcycle
   {
     public double BatteryKWh { get; set; }
-    public string MainColor { get; set; }
-    public int MaximumOccupancy { get; set; }
+
 
     public void ChargeBattery()
     {
